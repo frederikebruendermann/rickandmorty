37472454e31e1d5ede98a3ce6b19c183c43cfdc6
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-05-14 22:32:50 +0200

    Add nav bar basis

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Card from "./Card";
-import Navigation from "./Navigation";
+import Navigation from "./Navigation.js";
 import logo from "../src/images/logo.png";
 import CardLocations from "./CardLocations";
 
@@ -29,43 +29,49 @@ export default function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img className="HeaderImg" src={logo} alt=""></img>
-      </header>
+      <div className="CharacterCards hidden">
+        <header className="AppHeader">
+          <img className="HeaderImg" src={logo} alt=""></img>
+          <h1>- Characters - </h1>
+        </header>
+        {characters.map((character) => {
+          const { name, status, species, gender, origin, location, image, id } =
+            character;
+          return (
+            <Card
+              key={id}
+              name={name}
+              status={status}
+              species={species}
+              gender={gender}
+              origin={origin.name}
+              location={location.name}
+              image={image}
+            />
+          );
+        })}
+      </div>
 
-      {characters.map((character) => {
-        const { name, status, species, gender, origin, location, image, id } =
-          character;
-        return (
-          <Card
-            key={id}
-            name={name}
-            status={status}
-            species={species}
-            gender={gender}
-            origin={origin.name}
-            location={location.name}
-            image={image}
-          />
-        );
-      })}
-
-      {locations.map((location) => {
-        const { id, name, type, dimension } = location;
-        return (
-          <CardLocations
-            key={id}
-            name={name}
-            type={type}
-            dimension={dimension}
-          />
-        );
-      })}
+      <div className="LocationCards hidden">
+        <header className="AppHeader">
+          <img className="HeaderImg" src={logo} alt=""></img>
+          <h1>- Locations - </h1>
+        </header>
+        {locations.map((location) => {
+          const { id, name, type, dimension } = location;
+          return (
+            <CardLocations
+              key={id}
+              name={name}
+              type={type}
+              dimension={dimension}
+            />
+          );
+        })}
+      </div>
 
       <section className="NavigationBox">
-        <Navigation>Characters</Navigation>
-        <Navigation>Locations</Navigation>
-        <Navigation>Episodes</Navigation>
+        <Navigation />
       </section>
     </div>
   );
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,28 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./Navigation.css";
+import Button from "./Button";
 
-export default function Navigation({ children }) {
-  return <nav className="Navigation">{children}</nav>;
+export default function Navigation() {
+  const [isActive, setIsActive] = useState({
+    characters: true,
+    locations: false,
+  });
+
+  return (
+    <nav className="Navigation">
+      <Button isActive={isActive.characters} handleClick={handleClick}>
+        characters
+      </Button>
+      <Button isActive={isActive.locations} handleClick={handleClick}>
+        locations
+      </Button>
+    </nav>
+  );
+
+  function handleClick(event) {
+    const value = event.target.name.toLowerCase();
+    const obj = { characters: false, locations: false };
+    obj[value] = true;
+    setIsActive(obj);
+  }
 }
